470db6539270bc327547603932388cc4b32be1b6
masthanvali <masthan549@gmail.com>
2019-08-15 13:28:04 +0530

    Update ReadConfiguration.py
    Initial checkin

--- src/ReadConfiguration.py
+++ src/ReadConfiguration.py
@@ -4,16 +4,23 @@ try:
     emailID_from = open("Configuration.txt").readlines()[0]
     emailID_to = open("Configuration.txt").readlines()[1]
     pwd = open("Configuration.txt").readlines()[2]
-    sheetName = open("Configuration.txt").readlines()[3]
-    sheetName = open("Configuration.txt").readlines()[3]    
-
+    workbookName = open("Configuration.txt").readlines()[3]
+    sheetName = open("Configuration.txt").readlines()[4]
+    appNameInSheetColumn = open("Configuration.txt").readlines()[5]
+    dateOfExpOfApplication = open("Configuration.txt").readlines()[6]
+    appExp = open("Configuration.txt").readlines()[7]
+                   
     userFromEmailId = emailID_from.split("::")[1].strip()
     userToEmailId = emailID_to.split("::")[1].strip() 
     userPwd = pwd.split("::")[1].strip()
-    LicenseSpreadSheetLoc = sheetName.split("::")[1].strip()
+    ApplicationExpiryWorkbookLoc = workbookName.split("::")[1].strip()
+    sheetNameInWorkbook = sheetName.split("::")[1].strip()
+    appNameColumnInWorkbook = appNameInSheetColumn.split("::")[1].strip()
+    appExpDate = dateOfExpOfApplication.split("::")[1].strip()
+    appExpiryDaysBefore = appExp.split("::")[1].strip()
     
     #Check if spread sheet exist
-    if not os.path.exists(LicenseSpreadSheetLoc):
+    if not os.path.exists(ApplicationExpiryWorkbookLoc):
         print("License file doesnt exist in given path!")
 
 except Exception as e:
